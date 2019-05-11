a3b792880f536ae6e93dd43763851d73cb432b2e
dlqs <donaldleeqs@gmail.com>
2019-05-11 23:34:51 +0800

    setup initial with one note

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Note from './components/Note.js';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          My Note Taking app
+        </header>
+
+        <div className="NoteContainer">
+          <Note />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/components/Note.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Note extends React.Component {
+  render() {
+    return (
+      <div className="Note">
+        <p>
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying 
+          out print, graphic or web designs. The purpose of 
+          lorem ipsum is to create a natural looking block of text 
+          (sentence, paragraph, page, etc.) that doesn't distract from the layout.
+          The passage experienced a surge in popularity when it was first
+          used on dry-transfer sheets. The passage is attributed 
+          to a typesetter back in nineteen ninety eight when the 
+          undertaker threw mankind off hеll in a cell, and plummeted 
+          sixteen feet through an announcer's table.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Note;
