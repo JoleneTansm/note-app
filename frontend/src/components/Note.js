f95c19b6a47d1bdd7ae6d0d759105ec96a05d5a7
dlqs <donaldleeqs@gmail.com>
2019-05-12 18:12:30 +0800

    replace plceholder with state text

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 class Note extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ''
+    }
+  }
+
   render() {
     return (
       <div className="Note">
         <b>ID: { this.props.id } </b>
         <p>
-          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying 
-          out print, graphic or web designs. The purpose of 
-          lorem ipsum is to create a natural looking block of text 
-          (sentence, paragraph, page, etc.) that doesn't distract from the layout.
-          The passage experienced a surge in popularity when it was first
-          used on dry-transfer sheets. The passage is attributed 
-          to a typesetter back in nineteen ninety eight when the 
-          undertaker threw mankind off hеll in a cell, and plummeted 
-          sixteen feet through an announcer's table.
+          { this.state.text }
         </p>
       </div>
     );
